e044b326ba6c5fffaa227ad05e1e2af829c3d4ff
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-02-16 03:37:17 +0300

    Enhance home controller functionality and implement product_detail view with URL registration

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import contacts, home, product_detail
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("home/", home, name="home"), path("contacts/", contacts, name="contacts")]
+urlpatterns = [
+    path("home/", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
+]
