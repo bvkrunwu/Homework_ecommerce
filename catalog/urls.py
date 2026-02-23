13f0b764ecb724bc854bb517c9a6e9f4fe1cbf0a
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-02-23 03:41:56 +0300

    Migrate catalog app controllers to Django Class-Based Views (CBV) architecture

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product_detail
+from catalog.views import ContactsTemplateView, ProductDetailView, ProductListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
