0cb19d88814cde834b9bce51504134a54588a45b
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-02-25 01:35:27 +0300

    Adjust urlpatterns paths for correct routing

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_list"),
-    path("<int:pk>", BlogDetailView.as_view(), name="blog_detail"),
+    path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("create/", BlogCreateView.as_view(), name="blog_create"),
     path("<int:pk>/update/", BlogUpdateView.as_view(), name="blog_update"),
     path("<int:pk>/delete/", BlogDeleteView.as_view(), name="blog_delete"),
--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls", namespace="catalog")),
-    path("blog/", include("blog.urls", namespace="blog")),
+    path("blogs/", include("blog.urls", namespace="blog")),
 ]
