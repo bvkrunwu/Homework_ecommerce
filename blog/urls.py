aadb01c314619fdc2eb794e23e31fb5f8891ef66
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-02-24 03:59:49 +0300

    Implement full CRUD for Blog model using Class-Based Views (CBV)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from catalog.apps import CatalogConfig
+from blog.apps import BlogConfig
+from blog.views import BlogCreateView, BlogDeleteView, BlogDetailView, BlogListView, BlogUpdateView
 
-from blog.views import
+app_name = BlogConfig.name
 
-app_name = CatalogConfig.name
-
-urlpatterns = []
+urlpatterns = [
+    path("", BlogListView.as_view(), name="blog_list"),
+    path("<int:pk>", BlogDetailView.as_view(), name="blog_detail"),
+    path("create/", BlogCreateView.as_view(), name="blog_create"),
+    path("<int:pk>/update/", BlogUpdateView.as_view(), name="blog_update"),
+    path("<int:pk>/delete/", BlogDeleteView.as_view(), name="blog_delete"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ("title", "content", "preview_image")
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ("title", "content", "preview_image")
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blog_list")
