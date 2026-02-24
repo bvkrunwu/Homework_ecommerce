280ee33ba4bde203e02bec53291806c3a227d08d
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-02-25 01:40:26 +0300

    Improve blog post functionality: - Increment article view count upon opening individual post - Display only published articles in list view - Redirect user to updated article page after successful editing

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from blog.models import Blog
@@ -13,15 +13,27 @@ class BlogCreateView(CreateView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ("title", "content", "preview_image")
     success_url = reverse_lazy("blog:blog_list")
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", args=[self.kwargs.get("pk")])
 
 
 class BlogDeleteView(DeleteView):
