aaaf2222e630accc77162564bb68bf400b1ad598
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-03-09 18:24:27 +0300

    Add product form styling via __init__ method for design consistency

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,12 +1,22 @@
 from django.core.exceptions import ValidationError
-from django.forms import ModelForm
+from django.forms import BooleanField, ModelForm
 
 from catalog.models import Product
 
 FORBIDDEN_WORDS = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs["class"] = "form-check-input"
+            else:
+                fild.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
